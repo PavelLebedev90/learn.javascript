2202b2e065f8a0162b6403dbafcffa5fb2d1372a
Pavel Lebedev <pashoktver90@gmail.com>
2021-10-23 20:50:25 +0300

    learning javaScript

---
+++ recursion2.js
@@ -0,0 +1,11 @@
+function rangeOfNumbers(startNum, endNum) {
+  if(startNum > endNum){
+    return [];
+  }else{
+    let arr = rangeOfNumbers(startNum, endNum-1);
+    arr.push(endNum);
+    return arr;
+  }
+  
+};
+rangeOfNumbers(2,5);
